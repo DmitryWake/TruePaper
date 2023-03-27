f704021103a9383b407abaf5c8d31cf003fc565c
Dmitry Wake <nikolaevskiy.work@gmail.com>
2023-03-27 21:28:37 +0300

    profile screen

--- app/src/main/java/com/ewake/truepaper/features/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/ewake/truepaper/features/profile/presentation/ProfileScreen.kt
@@ -1,7 +1,11 @@
 package com.ewake.truepaper.features.profile.presentation
 
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ewake.truepaper.features.profile.presentation.viewmodel.ProfileViewModel
 
 /**
@@ -10,5 +14,18 @@ import com.ewake.truepaper.features.profile.presentation.viewmodel.ProfileViewMo
 
 @Composable
 fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
+    val uiState: ProfileViewModel.ProfileScreenUiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    viewModel.init()
+
+    when (uiState) {
+        is ProfileViewModel.ProfileScreenUiState.Error -> {
+            Text(text = (uiState as ProfileViewModel.ProfileScreenUiState.Error).message)
+        }
+        ProfileViewModel.ProfileScreenUiState.Loading -> CircularProgressIndicator()
+        is ProfileViewModel.ProfileScreenUiState.Profile -> {
+            val profile = uiState as ProfileViewModel.ProfileScreenUiState.Profile
+            Text(text = "Ваш айди: ${profile.userId}\nДата регистрации: ${profile.registerDate}")
+        }
+    }
 }
--- app/src/main/java/com/ewake/truepaper/features/profile/presentation/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/ewake/truepaper/features/profile/presentation/viewmodel/ProfileViewModel.kt
@@ -1,12 +1,63 @@
 package com.ewake.truepaper.features.profile.presentation.viewmodel
 
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ewake.truepaper.R
+import com.ewake.truepaper.core.domain.repository.UserRepository
+import com.ewake.truepaper.features.splash.presentation.viewmodel.SplashScreenUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 /**
  * @author Nikolaevskiy Dmitriy
  */
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    @ApplicationContext
+    private val context: Context,
+    private val userRepository: UserRepository,
+) : ViewModel() {
+
+    private var _uiState: ProfileScreenUiState by mutableStateOf(ProfileScreenUiState.Loading)
+    val uiState: StateFlow<ProfileScreenUiState> = snapshotFlow { _uiState }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = _uiState
+    )
+
+    fun init() {
+        viewModelScope.launch {
+            _uiState = ProfileScreenUiState.Loading
+            runCatching { userRepository.getCurrentUser() }
+                .onSuccess {
+                    _uiState =
+                        it?.let { ProfileScreenUiState.Profile(it.id, Date(it.registerDate)) }
+                            ?: ProfileScreenUiState.Error(context.getString(R.string.profile_null_error))
+                }
+                .onFailure {
+                    _uiState = ProfileScreenUiState.Error(it.message.orEmpty())
+                }
+        }
+    }
+
+    sealed interface ProfileScreenUiState {
+        object Loading : ProfileScreenUiState
+        data class Error(val message: String) : ProfileScreenUiState
+        data class Profile(
+            val userId: String,
+            val registerDate: Date
+        ) : ProfileScreenUiState
+    }
 }
